63c2e55d31ba763e3589647cd5b9085bbb217daa
coder sparks <codersparks@gmail.com>
2015-12-12 21:58:55 +0000

    Update

---
+++ src/main/webapp/scripts/filters.js
@@ -0,0 +1,6 @@
+angularApp.filter('prettyJSON', function(){
+    function prettyPrintJson(json) {
+        return JSON ? JSON.stringify(json, null, '  ') : "Your Browser does not support JSON therefore cannot pretty print";
+    }
+    return prettyPrintJson;
+})
