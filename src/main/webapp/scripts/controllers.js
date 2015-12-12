e6d53fe1b98d6ea98ed46602d4ccb2cb5bfca42f
coder sparks <codersparks@gmail.com>
2015-12-12 21:40:29 +0000

    Update

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -6,11 +6,29 @@ angularApp.controller('mainController', function($scope) {
 
 });
 
-angularApp.controller('hackingController', function($scope){
+angularApp.controller('hackingController', function($scope, $http){
     $scope.Model = $scope.Model || {}
     $scope.Model.showSessionData = false;
 
     $scope.Model.newSession = function(possibleSolutions) {
+        console.log("Model.newSession called")
+        console.log("Possible Solutions: " + possibleSolutions);
+
+        var possibleSolutionsText = [];
+
+        for(i=0; i<possibleSolutions.length; i++) {
+            possibleSolutionsText.push(possibleSolutions[i].text)
+        }
+        possibleSolutionsJson = JSON.stringify(possibleSolutionsText)
+        console.log("Possible Solutions (text): " + possibleSolutionsJson)
+
+        $http.post('/api/session/', possibleSolutionsJson)
+            .success(function successFunction(data) {
+                console.log("Success: " + data);
+            }).error(function errorFunction(data) {
+                console.log("Error: ")
+                console.log(data);
+            });
 
     }
 });
